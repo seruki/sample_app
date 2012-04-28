ab8775d525de455bc2e7354c0f47ab03ec3cf0e7
Liz Tabor <kittisketches@gmail.com>
2012-04-28 02:01:26 -0400

    Finishing About page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -8,5 +8,21 @@ require 'spec_helper'
          visit '/static_pages/home'
          page.should have_content('Sample App')
       end
-   end
+    end
+
+    describe "Help page" do
+      it "should have the content 'Help'" do
+
+        visit '/static_pages/help'
+        page.should have_content('Help')
+      end
+    end
+
+    describe "About page" do
+      it "should have the content 'About Us'" do
+
+        visit '/static_pages/about'
+        page.should have_content('About Us')
+      end
+    end
   end
